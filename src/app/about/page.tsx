7435db406721181828a73ec1f749e2a27effd484
nahueRosso <enzonahuelrojo000@gmail.com>
2024-12-18 12:23:40 -0300

    feat: uiImplement

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">Welcome to Cammon Sense!</h1>
+        <p className="text-lg mt-4 text-gray-700">
+          This is a simple Next.js page to get started.
+        </p>
+        <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Get Started
+        </button>
+      </main>
+    );
+  }
+  
